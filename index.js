2a461554e7e63a3b9b46ed02458481201536c8f9
Ksenia Peresedova <kshusya.yay@gmail.com>
2023-12-02 02:04:17 +0400

    feat: change lang, swap lang
    refactor: make separate function updateCharactersCount

--- index.js
+++ index.js
@@ -1,29 +1,93 @@
-function getData() {
-    fetch('https://api.mymemory.translated.net/get?q=Hello,%20how%20are%20you?!&langpair=en|fr')
-        .then(response => response.json())
-        .then(data => {
-            console.log(data)
-        })
-        .catch(error => {
-            console.log(error)
-        })
-
-}
-// getData();
-
-// User input
 const textarea = document.getElementById('text-to-translate');
+const paragraph = document.getElementById('translated');
 
-const countOfDigit = document.getElementById('digit-count');
+const countOfCharacters = document.getElementById('characters-count');
+
+const languagesButtons = document.querySelectorAll('.languages__parameter');
+const inputLanguagesButtons = Array.from(document.querySelectorAll('.to-translate .languages__parameter'));
+const outputLanguagesButtons = Array.from(document.querySelectorAll('.translated .languages__parameter'));
+
+function updateCharactersCount() {
+    countOfCharacters.textContent = `${textarea.value.length}/500`;
+}
+
+inputLanguagesButtons.forEach((button) => {
+    button.addEventListener('click', changeLanguage);
+});
+outputLanguagesButtons.forEach((button) => {
+    button.addEventListener('click', changeLanguage);
+});
+
+function changeLanguage(event) {
+    const languagesButtons = Array.from(event.currentTarget.parentNode.querySelectorAll('.languages__parameter'));
+
+    const inputButton = document.querySelector('.to-translate .languages__parameter.active');
+    const inputLang = inputButton.dataset.language;
+    const outputButton = document.querySelector('.translated .languages__parameter.active');
+    const outputLang = outputButton.dataset.language;
+    
+    const clickedButton = event.target;
+
+    // if selected language is pressed repeatedly, the next following code doesn't make swap
+    if (clickedButton === inputButton || clickedButton === outputButton) return;
+
+    // if clicked language is already active, make language swap
+    if (clickedButton.dataset.language === inputLang || clickedButton.dataset.language === outputLang) {
+        swapLanguages();
+        return;
+    }
+
+    // remove active classes
+    languagesButtons.forEach((button) => {
+        button.classList.remove('active');
+    })
+    
+    // add active class on selected button
+    clickedButton.classList.add('active');
+}
+
+const reverseButton = document.getElementById('reverse-button');
+reverseButton.addEventListener('click', swapLanguages);
+
+function swapLanguages() {
+    const inputLangButton = document.querySelector('.to-translate .languages__parameter.active');
+    const outputLangButton = document.querySelector('.translated .languages__parameter.active');
+
+    // remove active classes
+    inputLangButton.classList.remove('active');
+    outputLangButton.classList.remove('active');
+
+    // make language swap
+    document.querySelector(`.translated .languages__parameter[data-language="${inputLangButton.dataset.language}"]`).classList.add('active');
+    document.querySelector(`.to-translate .languages__parameter[data-language="${outputLangButton.dataset.language}"]`).classList.add('active');
+
+    // make text swap
+    const backup = textarea.value;
+    textarea.value = paragraph.textContent;
+    paragraph.textContent = backup;
+
+    updateCharactersCount();
+}
 
 function getTranslate() {
     const textToTranslate = textarea.value;
     const textLength = textToTranslate.length;
-    countOfDigit.textContent = `${textLength}/500`
+    updateCharactersCount();
+
+    // remove placeholder after user has typed the text
+    textarea.removeAttribute('placeholder');
+    // remove translated text after user has deleted the text
+    if (textLength === 0) {
+        postTranslate('');
+        return;
+    }
+
+    const inputLang = document.querySelector('.to-translate .languages__parameter.active').dataset.language;
+    const outputLang = document.querySelector('.translated .languages__parameter.active').dataset.language;
 
     const queryString = new URLSearchParams({
         q: textToTranslate,
-        langpair: 'en|fr',
+        langpair: `${inputLang}|${outputLang}`,
     }).toString();
 
     fetch(`https://api.mymemory.translated.net/get?${queryString}`)
@@ -36,17 +100,19 @@ function getTranslate() {
         .catch(error => {
             console.log(error)
         })
-
 }
 
+const translateButton = document.getElementById('translate-button');
+
+// translate triggers
+languagesButtons.forEach((button) => {
+    button.addEventListener('click', getTranslate);
+});
 textarea.addEventListener('keyup', getTranslate);
+translateButton.addEventListener('click', getTranslate);
 
 // add translated text to html
 function postTranslate(translatedText) {
-    const paragraph = document.getElementById('translated');
-
-    // if (translatedText.length === 0) paragraph.textContent ='Bonjour, comment allez-vous?';
-
     paragraph.textContent = translatedText;
 }
 
